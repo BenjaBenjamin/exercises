823ee573b3dd4011d22a97a68fdad2928a2ea9c1
Benjamin Worm <Benja_2@hotmail.dk>
2019-09-23 12:32:14 +0200

    get + post work

--- get.js
+++ get.js
@@ -9,14 +9,42 @@ function get() {
   })
     .then(e => e.json())
     .then(heroes => {
-      heroes.forEach(hero => {
-        const template = document.querySelector("template").content;
-        const copy = template.cloneNode(true);
-        copy.querySelector("h1").textContent = hero.name;
-        copy.querySelector("h2").textContent = hero.realname;
-        copy.querySelector("ul").textContent = hero.Powers;
-        document.querySelector("#app").appendChild(copy);
-      });
+      heroes.forEach(addNewHero);
     });
 }
 get();
+
+function addNewHero(hero) {
+  const template = document.querySelector("template").content;
+  const copy = template.cloneNode(true);
+  copy.querySelector("h1").textContent = hero.name;
+  copy.querySelector("h2").textContent = hero.realname;
+  copy.querySelector("ul").textContent = hero.Powers;
+  document.querySelector("#app").appendChild(copy);
+}
+
+function post() {
+  const data = {
+    name: "Batman",
+    realname: "Bruce Wayne",
+    age: 69,
+    Powers: "smart/fast"
+  };
+
+  const postData = JSON.stringify(data);
+  fetch("https://frontendautumn2019-883b.restdb.io/rest/superheroes", {
+    method: "post",
+    headers: {
+      "Content-Type": "application/json; charset=utf-8",
+      "x-apikey": "5d887444fd86cb75861e25ef",
+      "cache-control": "no-cache"
+    },
+    body: postData
+  })
+    .then(res => res.json())
+    .then(data => {
+      addNewHero(data);
+    });
+}
+
+document.querySelector("button").addEventListener("click", post);
